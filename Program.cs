aeedab1d83cb5fc77fb335b45bfc80de349e2717
ABHI <abhishekitte11@gmail.com>
2021-09-21 18:04:30 +0530

    [ABHISHEK] Added a Edit Contact

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 
 namespace AddressBook.cs
 {
-    class AddContact
-    {   //Uc2 to create contact in an address book
+    class AddressBook
+    {   //Uc3 to edit contact in an address book
+        //creating object person from Contact class
+
         public List<Contact> personDetails = new List<Contact>();
 
         //Creating a method for adding contacts in adrressbook
-        private void AddPerson()
+        public void AddPerson()
         {
-            //creating object person from Contact class
             Contact person = new Contact();
             Console.WriteLine("Enter first name");
             person.firstName = Console.ReadLine();
@@ -29,30 +30,92 @@ namespace AddressBook.cs
             Console.WriteLine("Enter email id");
             person.email = Console.ReadLine();
             personDetails.Add(person);
-            Print(person);
         }
 
         //Printing the address book details 
-        private void Print(Contact person)
+        public void Print()
         {
-            Console.WriteLine("---Address book details----");
-            Console.WriteLine("First Name:" + person.firstName);
-            Console.WriteLine("Last Name:" + person.lastName);
-            Console.WriteLine("Address:" + person.address);
-            Console.WriteLine("City:" + person.city);
-            Console.WriteLine("State:" + person.state);
-            Console.WriteLine("Zip:" + person.zip);
-            Console.WriteLine("Phone Number:" + person.phoneNumber);
-            Console.WriteLine("Email:" + person.email);
+            foreach (Contact person in personDetails)
+            {
+                Console.WriteLine("---Address book details----");
+                Console.WriteLine("First Name:" + person.firstName);
+                Console.WriteLine("Last Name:" + person.lastName);
+                Console.WriteLine("Address:" + person.address);
+                Console.WriteLine("City:" + person.city);
+                Console.WriteLine("State:" + person.state);
+                Console.WriteLine("Zip:" + person.zip);
+                Console.WriteLine("Phone Number:" + person.phoneNumber);
+                Console.WriteLine("Email:" + person.email);
+            }
         }
-
-        static void Main(string[] args)
+        public void Edit()
         {
-            Console.WriteLine("---Welcome to your Address Book--");
-            //creating object person1 of Addcontact Class()
-            AddContact person1 = new AddContact();
-            person1.AddPerson();
-            Console.ReadLine();
+            if (personDetails.Count != 0)
+            {
+                Console.WriteLine("Enter contact to modify:");
+                string edit = Console.ReadLine();
+                foreach (var person in personDetails)
+                {
+                    if (person.firstName.ToUpper() == edit.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the option to modify the property: ");
+                            Console.WriteLine("Enter 1 to Change First name ");
+                            Console.WriteLine("Enter 2 to Change Last name ");
+                            Console.WriteLine("Enter 3 to Change Phone Number ");
+                            Console.WriteLine("Enter 4 to Change Address ");
+                            Console.WriteLine("Enter 5 to Change City ");
+                            Console.WriteLine("Enter 6 to Change State ");
+                            Console.WriteLine("Enter 7 to Change Pincode ");
+                            Console.WriteLine("Enter 8 to Exit ");
+                            int Option = Convert.ToInt32(Console.ReadLine());
+                            switch (Option)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.firstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.lastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.phoneNumber = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.address = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.city = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.state = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.zip = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter the valid name!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your address book is empty");
+            }
         }
     }
 }
+
